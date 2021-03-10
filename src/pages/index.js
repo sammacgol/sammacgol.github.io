02c0104031678afbe4259f1e67e6462addcc91f3
ledpink <supertmania@naver.com>
2021-03-10 18:21:15 +0900

    ff

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,14 @@ import React from "react"
 //import { Carousel } from "react-bootstrap";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import CustomCarousel from "../components/customCarousel"
-//import Slider from "../components/slider"
+//import CustomCarousel from "../components/customCarousel"
+import Slider from "../components/slider"
 import IconBox from "../components/iconBox"
 
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" />
-    <CustomCarousel />
+    <Slider />
     <hr className="my-3 w-25" />
     <IconBox />
   </Layout>
